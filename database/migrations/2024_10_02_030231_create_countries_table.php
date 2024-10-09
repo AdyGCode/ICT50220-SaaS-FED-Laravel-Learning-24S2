f284df66799f4ab55da9db1073c9895d216c132a
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2024-10-09 15:38:57 +0800

    feat(countries): Update Countries with more details
    - Country seed data from https://github.com/dr5hn/countries-states-cities-database
    - Added numerous fields
    
    TODO:
     - Update Create, Edit, et al to include new data

--- database/migrations/2024_10_02_030231_create_countries_table.php
+++ database/migrations/2024_10_02_030231_create_countries_table.php
@@ -17,7 +17,23 @@ return new class extends Migration
             $table->string('iso3',3)->nullable();
             $table->string('iso2',2)->nullable();
             $table->string('tld',16)->nullable();
-            $table->string('dial_code',8)->nullable();
+            $table->integer('phone_code')->nullable();
+            $table->integer('numeric_code');
+            $table->string('capital')->nullable();
+            $table->string('currency',3)->nullable();
+            $table->string('currency_name')->nullable();
+            $table->string('currency_symbol')->nullable();
+            $table->string('native')->nullable();
+            $table->string('region')->nullable();
+            $table->integer('region_id')->nullable();
+            $table->string('subregion')->nullable();
+            $table->integer('subregion_id')->nullable();
+            $table->string('nationality')->nullable();
+            $table->json('timezones')->nullable();
+            $table->float("latitude")->nullable();
+            $table->float("longitude")->nullable();
+            $table->string("emoji")->nullable();
+            $table->string("emojiU")->nullable();
             $table->timestamps();
         });
     }
